a66872a773b9674ad3a2f8e13631f2f96b9f42bf
boss0077 <sudarshanrpgowda7@gmail.com>
2025-12-08 12:14:45 +0530

    Remove Fluent Bit - not needed

--- terraform/fluent-bit.tf
+++
@@ -1,7 +0,0 @@
-# Fluent Bit for CloudWatch Logs
-resource "aws_eks_addon" "aws_for_fluent_bit" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "aws-for-fluent-bit"
-
-  depends_on = [aws_eks_node_group.main]
-}
