a6bd22e46f499d9f2bef3403997ba7f5886b6082
boss0077 <sudarshanrpgowda7@gmail.com>
2025-12-08 15:06:04 +0530

    actions v13

--- terraform/fluent-bit.tf
+++ terraform/fluent-bit.tf
@@ -3,5 +3,8 @@ resource "aws_eks_addon" "cloudwatch_observability" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "amazon-cloudwatch-observability"
 
-  depends_on = [aws_eks_node_group.main]
+  depends_on = [
+    aws_eks_node_group.main,
+    helm_release.alb_controller
+  ]
 }
