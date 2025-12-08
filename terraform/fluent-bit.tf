fc1f075e24456cf162995bbd31805fdc9b33db01
boss0077 <sudarshanrpgowda7@gmail.com>
2025-12-08 12:24:55 +0530

    actions v11

---
+++ terraform/fluent-bit.tf
@@ -0,0 +1,7 @@
+# Amazon CloudWatch Observability addon for container logs
+resource "aws_eks_addon" "cloudwatch_observability" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "amazon-cloudwatch-observability"
+
+  depends_on = [aws_eks_node_group.main]
+}
